84be209109697bf83eee030e33e8585a9e0990e9
iamaparnarajesh <aparna.sri85@gmail.com>
2020-08-05 21:40:42 +0800

    truffle-config file updated

--- blockchain/migrations/1_initial_migration.js
+++ blockchain/migrations/1_initial_migration.js
@@ -1,5 +1,8 @@
 const Migrations = artifacts.require("Migrations");
+const Supplychain = artifacts.require("Supplychain");
+
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(Supplychain);
 };
